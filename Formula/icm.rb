5df0a5f24a2a11ff6dd7de60467e2a63a8a8bc53
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-13 14:43:09 +0200

    Brew formula update for icm version 1.0.0-beta

--- Formula/icm.rb
+++ Formula/icm.rb
@@ -1,12 +1,19 @@
 class Icm < Formula
   desc "Validate or generate intermodal container markings."
   homepage "https://github.com/meyermarcel/icm"
-  url "https://github.com/meyermarcel/icm/releases/download/v0.0.0/icm_Darwin_x86_64.tar.gz"
-  version "SNAPSHOT-f681258a36e811bb7ec227a7880a4089a34f83e2"
-  sha256 "ec0c7e20d240512127457506c69247670dfe2ef0d66a66a18c9bbac4dd1769c1"
+  url "https://github.com/meyermarcel/icm/releases/download/1.0.0-beta/icm_Darwin_x86_64.tar.gz"
+  version "1.0.0-beta"
+  sha256 "39c3fe92b7a0fc615bc4314119f739980711dc22eb99ef5d65324b9c61c6c538"
 
   def install
     bin.install "icm"
+    # Install bash completion
+    output = Utils.popen_read("#{bin}/icm completion bash")
+    (bash_completion/"icm").write output
+    
+    # Install zsh completion
+    output = Utils.popen_read("#{bin}/icm completion zsh")
+    (zsh_completion/"_icm").write output
   end
 
   test do
